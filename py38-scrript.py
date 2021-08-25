ea4e67b9890c985bdf4fa613a79e0032cd13551d
admin <-->
2021-08-25 05:49:01 +0000

    py38

---
+++ py38-scrript.py
@@ -0,0 +1,9 @@
+import sys
+print ("The script has the name %s" % (sys.argv[0]))
+arguments = len(sys.argv) - 1
+print ("The script is called with %i arguments" % (arguments))
+
+# Get environment variables
+import os
+ENV1 = os.getenv('ENV1')
+ENV2 = os.getenv('ENV2')

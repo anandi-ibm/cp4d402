7fb657b3b902951ef6171517c3dd1d22e54f5017
admin <-->
2021-09-24 06:54:50 +0000

    py38

---
+++ assets/py38-env-arg.py
@@ -0,0 +1,11 @@
+# Get environment variables
+import os
+ENV1 = os.getenv('ENV1')
+ENV2 = os.getenv('ENV2')
+ 
+#script with arg
+
+import sys
+print ("The script has the name %s" % (sys.argv[0]))
+arguments = len(sys.argv) - 1
+print ("The script is called with %i arguments" % (arguments))
